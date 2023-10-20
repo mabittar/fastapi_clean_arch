c07ed1fe0cc28ed335db0436a3fcbbaf2f5e4bf7
Marcel Bittar <ma_bittar@yahoo.com.br>
2023-10-20 17:25:33 -0300

    feat: persist data

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 # Crie uma instância do FastAPI
 app = FastAPI()
+
+items = {}
+
+def find_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return None
 
 # Crie uma rota simples
 @app.get("/")
@@ -11,21 +19,37 @@ def read_root():
 # Crie uma rota com parâmetros
 @app.get("/items/{item_id}")
 def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+    memory_item = find_item(item_id)
+    if not memory_item:
+        raise HTTPException(status_code=404, detail="Item not exists")
+    return memory_item
 
 # Rota de criação de itens
 @app.post("/items/")
 def create_item(item: dict):
+    memory_item = find_item(item['item_id'])
+    if memory_item:
+        raise HTTPException(status_code=400, detail="Item already exists")
+    items[item['item_id']] = item
     return item
 
 # Rota de atualização de itens
 @app.put("/items/{item_id}")
 def update_item(item_id: int, updated_item: dict):
-    return {"item_id": item_id, "updated_item": updated_item}
+    memory_item = find_item(item_id)
+    if not memory_item:
+        raise HTTPException(status_code=404, detail="Item not exists")
+    items[item_id] = updated_item
+    
+    return {"item_id": item_id, "updated_item": items[item_id] }
 
 # Rota de exclusão de itens
 @app.delete("/items/{item_id}")
 def delete_item(item_id: int):
+    memory_item = find_item(item_id)
+    if not memory_item:
+        raise HTTPException(status_code=404, detail="Item not exists")
+    items.pop(item_id)
     return {"message": f"Item {item_id} excluído com sucesso"}
 
 if __name__ == "__main__":

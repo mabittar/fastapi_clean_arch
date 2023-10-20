87e731bfb83363eb817c9313fee2ea2f3182cc05
Marcel Bittar <ma_bittar@yahoo.com.br>
2023-10-20 17:25:56 -0300

    test: test all routes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+from random import randint
 import unittest
 
 from fastapi.testclient import TestClient
@@ -8,17 +9,73 @@ client = TestClient(app)
 
 
 class TestApp(unittest.TestCase):
-    def test_app_get(self):
-        response = client.get("/")
-        self.assertEqual(response.status_code, 200)
-        # item_data = {"item_id": 1, "name": "Test Item", "email": "test@example.com"}
-        # response = client.post("/items/", json=item_data)
-        # assert response.status_code == 200
-        # response_data = response.json()
-        # assert response_data["item"]["item_id"] == 1
-        # assert response_data["item"]["name"] == "Test Item"
-        # assert response_data["item"]["email"] == "test@example.com"
+    @staticmethod
+    def make_valid_body():
+        return {
+            "item_id": randint(1, 1000), 
+            "name": "Test Item", 
+            "email": "test@example.com"
+            }
 
+    def test_app_root(self):
+        result = client.get("/")
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.json(), {"message": "Bem-vindo ao FastAPI!"})
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_app_post(self):
+        item_data = self.make_valid_body()
+        result = client.post("/items/", json=item_data)
+        self.assertEqual(result.status_code , 200)
+        result_response = result.json()
+        self.assertEqual(result_response["item_id"] , item_data['item_id'])
+        self.assertEqual(result_response["name"] , item_data['name'])
+        self.assertEqual(result_response["email"] , item_data['email'])
+
+    def test_same_itemId_must_raise_error(self):
+        item_data = self.make_valid_body()
+        client.post("/items/", json=item_data)
+        result = client.post("/items/", json=item_data)
+        self.assertEqual(result.status_code, 400)
+
+    def test_get_item(self):
+        item_data = self.make_valid_body()
+        client.post("/items/", json=item_data)
+        result = client.get(f"/items/{ item_data['item_id']}")
+        self.assertEqual(result.status_code , 200)
+        result_response = result.json()
+        self.assertEqual(result_response["item_id"] , item_data['item_id'])
+        self.assertEqual(result_response["name"] , item_data['name'])
+        self.assertEqual(result_response["email"] , item_data['email'])
+
+    def test_get_invaid_item_must_rise_error(self):
+        result = client.get(f"/items/-1")
+        self.assertEqual(result.status_code , 404)
+
+    def test_delete_item(self):
+        item_data = self.make_valid_body()
+        client.post("/items/", json=item_data)
+        item_id = item_data['item_id']
+        result = client.delete(f"/items/{item_id}")
+        self.assertEqual(result.status_code, 200)
+        result_response = result.json()
+        self.assertEqual(result_response['message'], f"Item {item_id} excluído com sucesso")
+
+    def test_delete_invaid_item_must_rise_error(self):
+        result = client.get(f"/items/-1")
+        self.assertEqual(result.status_code , 404)
+
+    def test_update_item(self):
+        item_data = self.make_valid_body()
+        client.post("/items/", json=item_data)
+        item_data['name'] = 'new_name'
+        result = client.put(f"/items/{ item_data['item_id']}", json=item_data)
+        self.assertEqual(result.status_code , 200)
+        result_response = result.json()
+        self.assertEqual(result_response['updated_item']['name'], 'new_name')
+
+    def test_update_invalid_body_must_raise_error(self):
+        item_data = self.make_valid_body()
+        client.post("/items/", json=item_data)
+        result = client.put(f"/items/{ item_data['item_id']}")
+        self.assertEqual(result.status_code , 422)
+

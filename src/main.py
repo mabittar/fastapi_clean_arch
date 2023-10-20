90ca9ce7d2bffc52edf4c8ff4125ad22dfb96f7a
Marcel Bittar <ma_bittar@yahoo.com.br>
2023-10-20 18:58:53 -0300

    feat: add new pydantic basemodel item

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+# Usando Pydantic
+class Item(BaseModel):
+    item_id: int
+    name: str
+    email: str
 
 # Crie uma instância do FastAPI
 app = FastAPI()
@@ -26,21 +33,20 @@ def read_item(item_id: int, query_param: str = None):
 
 # Rota de criação de itens
 @app.post("/items/")
-def create_item(item: dict):
-    memory_item = find_item(item['item_id'])
+def create_item(item: Item):
+    memory_item = find_item(item.item_id)
     if memory_item:
         raise HTTPException(status_code=400, detail="Item already exists")
-    items[item['item_id']] = item
+    items[item.item_id] = item.model_dump()
     return item
 
 # Rota de atualização de itens
 @app.put("/items/{item_id}")
-def update_item(item_id: int, updated_item: dict):
+def update_item(item_id: int, updated_item: Item):
     memory_item = find_item(item_id)
     if not memory_item:
         raise HTTPException(status_code=404, detail="Item not exists")
-    items[item_id] = updated_item
-    
+    items[item_id] = updated_item.model_dump()    
     return {"item_id": item_id, "updated_item": items[item_id] }
 
 # Rota de exclusão de itens
@@ -52,6 +58,7 @@ def delete_item(item_id: int):
     items.pop(item_id)
     return {"message": f"Item {item_id} excluído com sucesso"}
 
+# Inicialização do arquivo Python
 if __name__ == "__main__":
     import uvicorn
 
